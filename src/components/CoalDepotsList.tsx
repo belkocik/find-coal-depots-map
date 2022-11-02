27db9df356fee6f27f66b89a60825b11c8b58706
bubuq3 <62507500+bubuq3@users.noreply.github.com>
2022-11-02 18:42:09 +0100

    fix: removed ml from not found icon

--- src/components/CoalDepotsList.tsx
+++ src/components/CoalDepotsList.tsx
@@ -38,7 +38,7 @@ const CoalDepotsList = ({ coalDepots, setHighligtedId, loading }: IProps) => {
   if (coalDepots.length === 0)
     return (
       <div className="text-lg font-semibold mt-2 flex flex-col items-center bg-nav p-6 rounded-lg">
-        <FaSearchLocation className="ml-8 w-12 h-12" />
+        <FaSearchLocation className="w-12 h-12" />
         <p className="text-xl mt-2">
           Zakres wyświetlanej mapy nie obejmuje żadnego składu opału.
         </p>
